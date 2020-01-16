2b8667606c4f5c08b0ca34a6b16326983c90cbed
Joshua Dowding <joshuadowding@outlook.com>
2020-01-16 21:18:13 +0000

    add post_install script to meson build

---
+++ meson/post_install.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from os import environ, path
+from subprocess import call
+
+prefix = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
+datadir = path.join(prefix, 'share')
+destdir = environ.get('DESTDIR', '')
+
+if not destdir:
+    #print('Installing application icon...')
+    #call(['xdg-icon-resource', 'install', '--size', '48', path.join(datadir, 'ukuu', 'images', 'ukuu.png'), 'ukuu'])
+
+    print('Updating icon cache...')
+    call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
+
+    print('Updating desktop database...')
+    call(['update-desktop-database', '-q', path.join(datadir, 'applications')])
