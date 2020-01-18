4dbdad77e23cef8cbb44eb8fe3566b7ec8d9224c
Joshua Dowding <joshuadowding@outlook.com>
2020-01-18 05:18:22 -0800

    update build script

--- build-deb.sh
+++ build-deb.sh
@@ -4,42 +4,38 @@ backup=`pwd`
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "$DIR"
 
+build_source_for_dist() {
+    version=$1
+
+    debuild -i -us -uc -S
+
+    cd ..
+
+    mv -vf noyau_${version}.dsc noyau/release
+    mv -vf noyau_${version}.tar.xz noyau/release
+    mv -vf noyau_${version}_source.build noyau/release
+    mv -vf noyau_${version}_source.buildinfo noyau/release
+    mv -vf noyau_${version}_source.changes noyau/release
+
+    if [ $? -ne 0 ]; then cd "$backup"; echo "Failure, again!"; exit 1; fi
+
+    echo "--------------------------------------------------------------------------"
+}
+
 build_deb_for_dist() {
     dist=$1
     arch=$2
     version=$3
 
-    echo ""
-    echo "=========================================================================="
-    echo " build-deb.sh (${dist}:${arch}:${version})"
-    echo "=========================================================================="
-    echo ""
-
-    cd release
-
-    mkdir -pv source/${dist}
-
-    cd ..
-    cd ..
-
-    dpkg-source --build noyau
-
-    mv -vf noyau_${version}.dsc noyau/release/source/${dist}
-    mv -vf noyau_${version}.tar.xz noyau/release/source/${dist}
-
-    if [ $? -ne 0 ]; then cd "$backup"; echo "Failure, again!"; exit 1; fi
-
-    echo "--------------------------------------------------------------------------"
-
     cd noyau
 
-    ls -l release/source/${dist}
+    ls -l release/${dist}/${arch}
 
     mkdir -pv release/${dist}/${arch}
 
     echo "-------------------------------------------------------------------------"
 
-    sudo -S pbuilder --build --distribution ${dist} --architecture ${arch} --basetgz /var/cache/pbuilder/${dist}-${arch}-base.tgz release/source/${dist}/noyau_${version}.dsc
+    sudo -S pbuilder --build --distribution ${dist} --architecture ${arch} --basetgz /var/cache/pbuilder/${dist}-${arch}-base.tgz release/noyau_${version}.dsc
 
     if [ $? -ne 0 ]; then cd "$backup"; echo "Failure, again!"; exit 1; fi
 
@@ -60,18 +56,31 @@ build_deb_for_dist() {
     sudo rm -r /var/cache/pbuilder/result/*
 }
 
+echo ""
+echo "============"
+echo "build-deb.sh"
+echo "============"
+echo ""
+
 mkdir release
+cd release
+
+mkdir -pv ${dist}/${arch}
+
+cd ..
+
+build_source_for_dist 18.10
 
 #build_deb_for_dist xenial i386 18.10  #16.04.x LTS (not supported)
 #build_deb_for_dist xenial amd64 18.10 #16.04.x LTS (not supported)
 
-build_deb_for_dist bionic i386 18.10  #18.04.x LTS
+build_deb_for_dist bionic i386 18.10 #18.04.x LTS
 build_deb_for_dist bionic amd64 18.10 #18.04.x LTS
 
-build_deb_for_dist disco i386 18.10  #19.04
+build_deb_for_dist disco i386 18.10 #19.04
 build_deb_for_dist disco amd64 18.10 #19.04
 
-build_deb_for_dist eoan i386 18.10  #19.10
+build_deb_for_dist eoan i386 18.10 #19.10
 build_deb_for_dist eoan amd64 18.10 #19.10
 
 #build_deb_for_dist focal i386 18.10 #20.04.x LTS
